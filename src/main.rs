7be6142ccf0f711e4675afb37f6b4fe7831b98b2
khansakhai <kak.khansa@gmail.com>
2025-03-21 15:15:18 +0700

    (Bonus) Function improvement

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,17 @@ use std::{
     net::{TcpListener, TcpStream},
     thread,
     time::Duration,
+    process,
 };
 
 use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+    let pool = ThreadPool::build(4).unwrap_or_else(|err| {
+        println!("Pool size problem: {err}");
+        process::exit(1);
+    });
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
